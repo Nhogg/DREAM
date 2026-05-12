afc2b84dff45d670d99faa4d5278fa3e46a1b65d
Nathan Hogg <nathanhogg1223@gmail.com>
2026-05-12 18:43:02 +0000

    update with required data for basic training

--- data/DOWNLOAD.sh
+++ data/DOWNLOAD.sh
@@ -13,10 +13,10 @@ echo 'Warning:  Files are *very* large.  Be sure to comment out any files you do
 mkdir -p real
 cd real
 
-gdown --id 10Tpx8jAfzP6g44WXfvjlVywbIlxZ4BRx  # Panda-3Cam, Azure (368 MB)
-gdown --id 14TJ9o9QOdb25zlZ3onsOJlSb7-tGrvKz  # Panda-3Cam, Kinect360 (295 MB)
-gdown --id 1FFAFpJFwzsjD83S9-Y1ODwDWiWlh1X6P  # Panda-3Cam, RealSense (343 MB)
-gdown --id 1kL7Goibx4lwKQoO-UQ4gm94f_XdEKTUZ  # Panda-Orb, RealSense (2 GB)
+#gdown --id 10Tpx8jAfzP6g44WXfvjlVywbIlxZ4BRx  # Panda-3Cam, Azure (368 MB)
+#gdown --id 14TJ9o9QOdb25zlZ3onsOJlSb7-tGrvKz  # Panda-3Cam, Kinect360 (295 MB:
+#gdown --id 1FFAFpJFwzsjD83S9-Y1ODwDWiWlh1X6P  # Panda-3Cam, RealSense (343 MB)
+gdown 1kL7Goibx4lwKQoO-UQ4gm94f_XdEKTUZ  # Panda-Orb, RealSense (2 GB)
 
 cd ..
 
@@ -27,17 +27,17 @@ mkdir -p synthetic
 cd synthetic
 
 #----- Rethink Robotics' Baxter
-gdown --id 1MSRwQpg690RvuvtjNuGYA1ILGipX16dW  # test DR (541 MB)
-gdown --id 1SzUPYmNxe1OsbGyWdpdkoRjWJurs-NAF  # train (9 GB)
+#gdown --id 1MSRwQpg690RvuvtjNuGYA1ILGipX16dW  # test DR (541 MB)
+#gdown --id 1SzUPYmNxe1OsbGyWdpdkoRjWJurs-NAF  # train (9 GB)
 
 #----- Kuka LBR iiwa 7 R800
-gdown --id 1kGvSlVScmMohZStS-_NfCpCa5SBAcx_i  # test DR (445 MB)
-gdown --id 1ChF4jAGMPbPwe2dOZYPJ2t2rCSR0Xw9R  # test non-DR (222 MB)
-gdown --id 1HTW3YEGDO22zOT56jFWxfizznw4aGMpU  # train (8 GB)
+#gdown --id 1kGvSlVScmMohZStS-_NfCpCa5SBAcx_i  # test DR (445 MB)
+#gdown --id 1ChF4jAGMPbPwe2dOZYPJ2t2rCSR0Xw9R  # test non-DR (222 MB)
+#gdown --id 1HTW3YEGDO22zOT56jFWxfizznw4aGMpU  # train (8 GB)
 
 #----- Franka Emika Panda
-gdown --id 1tOzfzlRhUbRO-QkzPHvj-IOPFuSIts-R  # test DR (450 MB)
-gdown --id 11pK1BqfQkzVnTjyQHVRZ6ZkX4oyxbEQP  # test non-DR (190 MB)
-gdown --id 1ZXzseMa7aMIKxK4BNH2gacmm3_XGJvxm  # train (8 GB)
+gdown  1tOzfzlRhUbRO-QkzPHvj-IOPFuSIts-R  # test DR (450 MB)
+gdown  11pK1BqfQkzVnTjyQHVRZ6ZkX4oyxbEQP  # test non-DR (190 MB)
+gdown  1ZXzseMa7aMIKxK4BNH2gacmm3_XGJvxm  # train (8 GB)
 
 cd ..
